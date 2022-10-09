083fc44f8b605450e917c043fe211cbd6f0e797c
Marciel404 <103785110+Marciel404@users.noreply.github.com>
2022-10-09 13:56:43 -0300

    Create configs.py

---
+++ utils/configs.py
@@ -0,0 +1,160 @@
+configData = {
+
+  "token": "",
+
+  "mongokey": "",
+
+  "prefix": "",
+
+  "guild": ,
+  
+  "roles":{
+
+    "staff": {
+
+      "admin": ,
+
+      "mod": ,
+
+      "suporte": ,
+
+      "staff": 
+
+    },
+
+    "equipes":{
+
+      "equipecall": {
+
+        "submod": ,
+
+        "staffcall": ,
+
+        "movimenta": 
+
+      },
+
+      "equipechat": {
+        
+        "liderchat": ,
+
+        "staffchat": , 
+
+        "movimenta": 
+      },
+
+      "equipeeventos": {
+
+        "chefeeventos": ,
+
+        "apresentador": ,
+
+        "auxiliar": 
+
+      },
+
+      "equipediv": {
+
+        "promoters": ,
+
+        "div": 
+
+      },
+
+      "equipemidia": {
+
+        "chefemidia": ,
+
+        "equipemidia": 
+
+      }
+    },
+
+    "adv": {
+
+      "adv1": ,
+
+      "adv2": ,
+
+      "adv3": 
+
+    },
+
+    "outras":{
+
+      "standby": ,
+
+      "verificado": ,
+
+      "naoverificado": ,
+
+      "mute": ,
+
+      "ntb": ,
+
+      "nv100" : ,
+
+      "callpv": ,
+
+      "levelup": 
+
+    }
+
+  },
+
+  "logs":{
+
+    "chat": ,
+
+    "call": ,
+
+    "microfone": , 
+
+    "mod": ,
+
+    "membros": ,
+
+    "cargos": ,
+
+    "usocomandos": 
+
+  },
+
+  "chats":{
+
+    "ticket": ,
+
+    "ausencia": ,
+
+    "cmdstf": ,
+
+    "resumsemana": ,
+
+    "sugestsemana": ,
+
+    "oqprefere": ,
+
+    "ids": {
+
+      "cmdstf": ,
+
+      "tck": 
+
+    }
+
+  },
+  
+  "catego":{
+
+    "callpv": ,
+
+    "ticket": 
+
+  },
+
+  "calls":{
+    
+    "espera": 
+
+  }
+}
